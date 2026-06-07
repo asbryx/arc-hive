23bfd19c625543dbd7f6e09d5886dc61b3fe8698
asbryx <asbryx@users.noreply.github.com>
2026-06-07 19:18:07 +0000

    chore: add ESLint and Prettier configuration (T-06)

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: { node: true, es2022: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    'no-console': 'off',
+    'prefer-const': 'error',
+    'no-var': 'error',
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '*.js', '*.cjs', '!vitest.config.*'],
+}
